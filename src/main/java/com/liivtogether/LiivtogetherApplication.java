da2709451a7c8a14e16fe34b871a11164fe4186f
nudgelover <wlsgml2422@gmail.com>
2023-07-22 17:24:46 +0900

    first commit

---
+++ src/main/java/com/liivtogether/LiivtogetherApplication.java
@@ -0,0 +1,13 @@
+package com.liivtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiivtogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiivtogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liivtogether/LiivtogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liivtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiivtogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
