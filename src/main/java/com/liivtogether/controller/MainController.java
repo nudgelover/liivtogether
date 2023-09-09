311881fd403123dda15ef685c9b9a19f3e1d24ed
lssy2k1 <lssy2k1@gmail.com>
2023-09-09 23:17:14 +0900

    [23/09/09]카카오회원가입,로그인,암호화

--- src/main/java/com/liivtogether/controller/MainController.java
+++ src/main/java/com/liivtogether/controller/MainController.java
@@ -1,29 +1,47 @@
 package com.liivtogether.controller;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.liivtogether.dto.Cust;
 import com.liivtogether.service.CustService;
+import com.liivtogether.service.KakaoLoginService;
 
 @Controller
 public class MainController {
 	
+	@Value("${app.kakaorest}")
+	String kakaorest;	
+	
+	@Value("${app.kakaojs}")
+	String kakaojs;
+	
 	@Autowired
 	CustService custService = new CustService();
 	
+	@Autowired
+	KakaoLoginService kakaoLoginService = new KakaoLoginService();
+	
     @RequestMapping("/")
     public String main(){
-    	 
         return "index";
     }
     
     @RequestMapping("/login")
     public String login(Model model){
+    	model.addAttribute("kakaojs", kakaojs);
         return "login";
     }
     
@@ -36,6 +54,7 @@ public class MainController {
     
     @RequestMapping("/register")
     public String register(Model model){
+    	model.addAttribute("kakaojs", kakaojs);
     	model.addAttribute("center", "register");
         return "index";
     }
@@ -52,5 +71,62 @@ public class MainController {
     	return "index";
     }
     
+    // 카카오 로그인창 호출(ajax로는 호출 안됨)
+    @RequestMapping(value = "/login/getKakaoAuthUrl")
+    public @ResponseBody String getKakaoAuthUrl(HttpServletRequest request) throws Exception {
+    	String reqUrl = "https://kauth.kakao.com/oauth/authorize?client_id="+kakaorest+"&redirect_uri=http://localhost/kakaoLogin&response_type=code";
+    	return reqUrl;
+    }
+
+	// 1번 카카오톡에 사용자 코드 받기(jsp의 a태그 href에 경로 있음)
+    // 2번 받은 code를 iKakaoS.getAccessToken로 보냄 ###access_Token###로 찍어서 잘 나오면은 다음단계진행
+ 	// 3번 받은 access_Token를 iKakaoS.getUserInfo로 보냄 userInfo받아옴, userInfo에 nickname, email정보가 담겨있음
+	@RequestMapping(value = "/kakaoLogin", method = RequestMethod.GET)
+	public String kakaoLogin(@RequestParam(value = "code", required = false) String code, Model model, HttpSession session) throws Throwable {
+
+		// 1번
+		System.out.println("code:" + code);
+		
+		// 2번
+		String access_Token = kakaoLoginService.getAccessToken(code);
+		System.out.println("###access_Token#### : " + access_Token);
+		// 위의 access_Token 받는 걸 확인한 후에 밑에 진행
+		
+		// 3번
+		HashMap<String, Object> userInfo = kakaoLoginService.getUserInfo(access_Token);
+		System.out.println("###nickname#### : " + userInfo.get("nickname"));
+		System.out.println("###email#### : " + userInfo.get("email"));
+		
+		// 4번
+		// 정보있는지 체크 -> 있으면 로그인, 없으면 회원가입 후 로그인
+		String id = userInfo.get("email").toString();
+		String name = userInfo.get("nickname").toString();
+		String birthday = userInfo.get("birthday").toString();
+		String pwd = UUID.randomUUID().toString();
+		
+		Cust cust = custService.get(id);
+		
+		//아이디가 있을 경우 바로 로그인
+		if(cust != null) {
+			id = cust.getCustId();
+//			System.out.println("cust 기존 정보 보유");
+//			System.out.println("id::::::::::"+id);
+			session.setMaxInactiveInterval(3600);
+            session.setAttribute("logincust", custService.get(id));
+			return "redirect:/";
+		}
+		
+		//아이디가 없을 경우 회원가입 후 로그인
+		//(#{custId}, #{custPwd}, #{custName}, #{phoneNo}, #{birth}, #{img}, #{pointree}, #{starcoin}, #{isMaster})
+		cust = new Cust(id, pwd, name, "0000000000", birthday, "default.jpg", 0, 0, "0");
+		custService.register(cust);
+//		System.out.println("cust 신규 등록 완료");
+		session.setMaxInactiveInterval(3600);
+        session.setAttribute("logincust", custService.get(id));
+        
+		return "redirect:/";
+
+	}
+
   
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -21,6 +21,7 @@
 <link rel="stylesheet" href="/assets/css/common.css" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="/assets/js/common.js"></script>
+
 <!-- Title -->
 <title>Liiv Together</title>
 <style>
